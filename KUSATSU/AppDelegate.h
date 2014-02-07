7d7b4a0476dc1ee5b29d9a0416dcbf7d95ed88aa
T.Egawa <Tatsuya@TEgawa-no-MacBook-Air.local>
2014-02-07 20:39:53 +0900

    Initial Commit

---
+++ KUSATSU/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KUSATSU
+//
+//  Created by T.Egawa on 2014/02/07.
+//  Copyright (c) 2014年 fisproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KUSATSU/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KUSATSU
+//
+//  Created by T.Egawa on 2014/02/07.
+//  Copyright (c) 2014年 fisproject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
